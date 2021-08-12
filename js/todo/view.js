2194b2293c7de98038fc94b440e7095ed6fa79fb
Egorych <mikhalevichegor@gmail.com>
2021-08-12 20:19:15 +0300

    Displaying task status

--- js/todo/view.js
+++ js/todo/view.js
@@ -12,10 +12,14 @@ export default class View {
     }
 
     renderTasks(taskObject) {
+
+        const completeClass = taskObject.status === 'done' ? 'completed' : ''
+        const checked = taskObject.status === 'done' ? 'checked' : ''
+
         const taskHTML = `<li class="todo-item">
                                 <label class="todo-item-label">
-                                        <input class="checkbox" type="checkbox" />
-                                        <span>${taskObject}</span>
+                                        <input class="checkbox" type="checkbox" ${checked}/>
+                                        <span class = "${completeClass}">${taskObject.text}</span>
                                         <button class="btn btn-secondary btn-sm">Удалить</button>
                                 </label>
                          </li>`
